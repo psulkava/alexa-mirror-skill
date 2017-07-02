91f8eb029a82208e66eb595b3c01f1ece23d3d87
Pietari Sulkava <ptsulk22@aol.com>
2017-07-02 13:09:57 -0400

    Added index.js with initial skill definition

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Alexa = require('alexa-sdk');
+
+const APP_ID = 'amzn1.ask.skill.9940f13b-9e08-43ae-a82d-579d83a40e40';
+
+const languageStrings = {
+    'en-US': {
+        translation: {
+            HELP_MESSAGE: 'You can ask me to take a selfie for you.',
+            HELP_REPROMPT: 'What can I help you with?',
+            STOP_MESSAGE: 'Goodbye.'
+        },
+    }
+}
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('Welcome to the smart mirror skill!');
+    },
+    'TakeSelfie': function () {
+        this.emit('Taking a picture. Say cheese!');
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = this.t('HELP_MESSAGE');
+        const reprompt = this.t('HELP_REPROMPT');
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+};
+
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};       
